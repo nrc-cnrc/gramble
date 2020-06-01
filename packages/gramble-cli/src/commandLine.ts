7d60aa2851f2a909f94b37be8b130deddd8742ca
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-01 15:31:50 -0600

    change: attempt to use tsconfig extend and project references

--- packages/gramble-cli/src/commandLine.ts
+++ packages/gramble-cli/src/commandLine.ts
@@ -1,4 +1,4 @@
-import {GTable, makeTable, flattenToJSON, getTierAsString, Project, TextDevEnvironment} from "@gramble/gramble/src"
+import {GTable, makeTable, flattenToJSON, getTierAsString, Project, TextDevEnvironment} from "@gramble/gramble"
 import {parse as papaparse, ParseResult} from 'papaparse';
 import {createReadStream, createWriteStream, existsSync} from 'fs';
 
