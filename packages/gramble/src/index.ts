80d1fa54573154a0dea02d5ee407cd6bd540de9a
roedoejet <aidanpine@shaw.ca>
2020-06-11 09:45:17 -0700

    feat(client): added gramble clientside code

--- packages/gramble/src/index.ts
+++ packages/gramble/src/index.ts
@@ -1,4 +1,5 @@
-import {GTable, makeTable, flattenToJSON, getTierAsString} from "./transducers"
-import {Project, TextDevEnvironment, BrowserDevEnvironment} from "./spreadsheet"
+import { GTable, makeTable, flattenToJSON, getTierAsString } from "./transducers"
+import { Project, TextDevEnvironment, BrowserDevEnvironment } from "./spreadsheet"
+import { ClientSideProject, fromURL, fromURLAsync } from './clientSide'
 
-export {GTable, makeTable, flattenToJSON, getTierAsString, Project, TextDevEnvironment, BrowserDevEnvironment};
+export { GTable, makeTable, flattenToJSON, getTierAsString, Project, TextDevEnvironment, BrowserDevEnvironment, ClientSideProject, fromURL, fromURLAsync };
