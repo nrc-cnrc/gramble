0389ac6dcafc374de5ca92411456f99dd307ffa7
Patrick Littell <Patrick.Littell@nrc-cnrc.gc.ca>
2021-08-17 12:59:29 -0400

    Removed abandoned sub-repos
    We were still carrying around the "version 1" language (the parser-combinator one) and packages that assumed that (like the Angular component), but we're now onto the third version of the language (the one based on Brzozowski derivatives), and it's a very different language.
    
    With Fineen's port of the Michif gramble from v1, there are no longer any live projects using v1, so it's time to remove it.

--- packages/gramble/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { GTable, makeTable, getTierAsString } from "./transducers"
-import { Project, TextProject, TextDevEnvironment, BrowserDevEnvironment } from "./spreadsheet"
-import { ClientSideProject, fromURL, fromURLAsync } from './clientSide'
-
-export { GTable, makeTable, getTierAsString, Project, TextProject, TextDevEnvironment, BrowserDevEnvironment, ClientSideProject, fromURL, fromURLAsync };
--- packages/ngx-gramble/projects/ngx-gramble/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-gramble
- */
-
-export * from './lib/ngx-gramble.service';
-export * from './lib/ngx-gramble.component';
-export * from './lib/ngx-gramble.module';
