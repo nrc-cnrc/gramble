601cc82b12753babb67b292aa34f3046f566aed0
littell <Patrick.Littell@nrc-cnrc.gc.ca>
2023-05-16 10:06:16 -0400

    Adding Priority tests
    We weren't testing Priority on its own, so that makes it dicey to refactor safely.
    
    I also added some tests for behavior that's beyond the current capabilities of Priority, but that I think are worth working towards:
    
        * It's not currently possible to nest Priority and have them both work.
    
        * It's not currently possible to express Priority partially, (i.e for a subset of known tapes).
    
    These don't just fail to get the right answer, they throw an unsimplified result exception.
    
    Not having these behaviors is ultimately the cause of the tricky and brittle logic right before constructExpr involving Priority, Collection, and Counts.  There might or might not be a Priority, but there can only be one and it has to be complete, so you have to find it before making another, and to make that easier we expect it in certain structural positions.
    
    If Priorities were properly nestable, this logic would become simple.  We'd not have to look for the existing Priority, just wrap the whole thing in a new Priority that handles any unhandled tapes.

---
+++ packages/parser/tests/testPriority.ts
@@ -0,0 +1,115 @@
+
+import {
+    CharSet, Epsilon, Grammar, Priority, Seq, Uni,
+} from "../src/grammars";
+
+import {
+    testSuiteName, logTestSuite, VERBOSE_TEST_L2,
+    t1, t2, t3,
+    testHasTapes, testHasVocab, testGrammar,
+} from "./testUtil";
+
+import {
+    StringDict, SILENT, VERBOSE_DEBUG,
+} from "../src/util";
+
+// File level control over verbose output
+const VERBOSE = VERBOSE_TEST_L2;
+
+describe(`${testSuiteName(module)}`, function() {
+
+    logTestSuite(this.title);
+
+    describe('1. Literal t1:hello, priority t1', function() {
+        let grammar: Grammar = t1("hello");
+        grammar = Priority(["t1"], grammar);
+        testHasTapes(grammar, ["t1"]);
+        testGrammar(grammar, [{t1: 'hello'}], VERBOSE_DEBUG);
+    });
+
+    describe('2a. t1:hello+t2:world, priority t1>t2', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"));
+        grammar = Priority(["t1", "t2"], grammar);
+        testHasTapes(grammar, ["t1", "t2"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world'}]);
+    });
+
+    describe('2b. t1:hello+t2:world, priority t2>t1', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"));
+        grammar = Priority(["t2", "t1"], grammar);
+        testHasTapes(grammar, ["t1", "t2"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world'}]);
+    });
+
+    describe('3a. t1:hello+t2:world+t3:!, priority t1>t2>t3', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"), t3("!"));
+        grammar = Priority(["t1", "t2", "t3"], grammar);
+        testHasTapes(grammar, ["t1", "t2", "t3"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world', t3: "!"}]);
+    });
+    
+    describe('3b. t1:hello+t2:world+t3:!, priority t1>t3>t2', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"), t3("!"));
+        grammar = Priority(["t1", "t3", "t2"], grammar);
+        testHasTapes(grammar, ["t1", "t2", "t3"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world', t3: "!"}]);
+    });
+    
+    describe('3c. t1:hello+t2:world+t3:!, priority t2>t1>t3', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"), t3("!"));
+        grammar = Priority(["t2", "t1", "t3"], grammar);
+        testHasTapes(grammar, ["t1", "t2", "t3"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world', t3: "!"}]);
+    });
+    
+    describe('3d. t1:hello+t2:world+t3:!, priority t2>t3>t1', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"), t3("!"));
+        grammar = Priority(["t2", "t3", "t1"], grammar);
+        testHasTapes(grammar, ["t1", "t2", "t3"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world', t3: "!"}]);
+    });
+    
+    describe('3e. t1:hello+t2:world+t3:!, priority t3>t1>t2', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"), t3("!"));
+        grammar = Priority(["t3", "t1", "t2"], grammar);
+        testHasTapes(grammar, ["t1", "t2", "t3"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world', t3: "!"}]);
+    });
+    
+    describe('3f. t1:hello+t2:world+t3:!, priority t3>t2>t1', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"), t3("!"));
+        grammar = Priority(["t3", "t2", "t1"], grammar);
+        testHasTapes(grammar, ["t1", "t2", "t3"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world', t3: "!"}]);
+    });
+
+    describe('4a. Partial priority: t1:hello+t2:world, priority t1', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"));
+        grammar = Priority(["t1"], grammar);
+        testHasTapes(grammar, ["t1", "t2"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world'}]);
+    });
+
+    describe('4b. Partial priority: t1:hello+t2:world, priority t2', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"));
+        grammar = Priority(["t2"], grammar);
+        testHasTapes(grammar, ["t1", "t2"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world'}]);
+    });
+    
+    describe('5a. Nested priority: t1:hello+t2:world, priority t1, priority t2', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"));
+        grammar = Priority(["t1"], grammar);
+        grammar = Priority(["t2"], grammar);
+        testHasTapes(grammar, ["t1", "t2"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world'}]);
+    });
+    
+    describe('5b. Nested priority: t1:hello+t2:world, priority t2, priority t1', function() {
+        let grammar: Grammar = Seq(t1("hello"), t2("world"));
+        grammar = Priority(["t2"], grammar);
+        grammar = Priority(["t1"], grammar);
+        testHasTapes(grammar, ["t1", "t2"]);
+        testGrammar(grammar, [{t1: 'hello', t2: 'world'}]);
+    });
+});
