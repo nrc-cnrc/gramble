1da44719fd2870c95835385d2ddfca6bd5b7334d
littell <Patrick.Littell@nrc-cnrc.gc.ca>
2023-11-14 13:32:25 -0500

    Making new tapes work with SingleTape
    (1) Like everything else that creates new structure, the handleSingleTapes pass had to be modified to work with new tapes, basically by calling tapify() whenever new structure is created.
    
    Note that while SingleTape creates Renames (and Lits/Dots with new tapeNames) and thus changes the tape structure, it only does so locally.  SingleTape didn't do the default tape calculation (union of childrens' tapes), it `.tapes` are just `{tapeName}`, and the result of this pass will have that same `.tapes`.  So from the global perspective nothing has changed.
    
    (2) While I was at it, I noticed we had no grammar tests for SingleTape, so I made some.
    
    (3a) The error message for `Embed.tapes.length != 1` was confusing in the case `Embed.tapes.length == 0`, so I split them up into two error messages.
    
    (3b) We were only catching that error when the child was an Embed, it was actually fine to say `SingleTape(Seq(t1("a"),t2("b")))`.  This never actually comes up in real source grammars, but it's still erroneous and should receive an error/fixup.  So now we're checking for it.  (No error when it's `tapes.length == 0`, though, that's not problematic.)

--- packages/parser/src/passes/handleSingleTapes.ts
+++ packages/parser/src/passes/handleSingleTapes.ts
@@ -42,37 +42,48 @@ export class HandleSingleTapes extends GrammarPass {
             case "lit":        return this.handleLiteral(g, env);
             case "dot":        return this.handleDot(g, env);
             case "embed":      return this.handleEmbed(g, env);
-            default:           return g.mapChildren(this, env);
+            default:           return this.handleDefault(g, env);
         }
+    }
+
+    handleDefault(g: Grammar, env: PassEnv): GrammarResult {
+
+        if (this.tapeName !== undefined && g.tapes.length > 1) {
+            // shouldn't be possible in real source grammars
+            return g.err("Multiple fields not allowed in this context",
+                `Only grammars with one field (e.g. just "text" but not any other fields) ` +
+                `can be embedded into a regex or rule context.`)
+                .bind(_ => new EpsilonGrammar().tapify(env));
+        }
+
+        return g.mapChildren(this, env);
     }
 
     handleEmbed(g: EmbedGrammar, env: PassEnv): GrammarResult {
         
-        if (this.tapeName == undefined) {
+        if (this.tapeName === undefined) {
             // we're not in singleTape env
             return g.mapChildren(this, env);
         }
 
-        if (g.tapes.length != 1) {
+        if (g.tapes.length == 0) {
+            return g.err("Embedding zero-field symbol",
+                `This embedded symbol has no fields (e.g. "text"), it should have exactly one.`)
+                .bind(_ => new EpsilonGrammar().tapify(env));
+        }
+
+        if (g.tapes.length > 1) {
             return g.err("Embedding multi-field symbol",
                 `Only grammars with one field (e.g. just "text" but not any other fields) ` +
                 `can be embedded into a regex or rule context.`)
-                .bind(_ => new EpsilonGrammar());
+                .bind(_ => new EpsilonGrammar().tapify(env));
         }
 
-        /*
-        let result: Grammar = g;
-        for (const tape of g.tapes) {
-            if (tape == this.tapeName) continue;
-            if (tape.startsWith(HIDDEN_TAPE_PREFIX)) continue;
-            const hiddenTapeName = `${HIDDEN_TAPE_PREFIX}H${this.replaceIndex++}`;
-            //console.log(`hiding ${g.name}.${tape} as ${hiddenTapeName}`);
-            result = new HideGrammar(result, tape, hiddenTapeName);
-        }
-        */
-        const gTapeName = g.tapes[0];
-        if (this.tapeName != gTapeName) {
-            return new RenameGrammar(g, gTapeName, this.tapeName).msg();
+        const embedTapeName = g.tapes[0];
+        if (this.tapeName != embedTapeName) {
+            return new RenameGrammar(g, embedTapeName, this.tapeName)
+                        .tapify(env)
+                        .msg();
         }
 
         return g.msg();
@@ -80,14 +91,18 @@ export class HandleSingleTapes extends GrammarPass {
 
     private handleDot(g: DotGrammar, env: PassEnv): GrammarResult {
         if (g.tapeName == DEFAULT_TAPE && this.tapeName != undefined) {
-            return new DotGrammar(this.tapeName).msg();
+            return new DotGrammar(this.tapeName)
+                        .tapify(env)
+                        .msg();
         }
         return g.msg();
     }
 
     private handleLiteral(g: LiteralGrammar, env: PassEnv): GrammarResult {
         if (g.tapeName == DEFAULT_TAPE && this.tapeName != undefined) {
-            return new LiteralGrammar(this.tapeName, g.text).msg();
+            return new LiteralGrammar(this.tapeName, g.text)
+                        .tapify(env)
+                        .msg();
         }
         return g.msg();
     }
