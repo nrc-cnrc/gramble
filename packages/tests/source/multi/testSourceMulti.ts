nrc-cnrc/gramble

commit 0357e14b50205ccedfb0b55a431a58d9e6e4f873

In the Sheets addon, display all error/warning messages for each cell in its note.
Error messages are prepended with "ERROR: "; warning messages are prepended with
"WARNING: "; success messages are prepended with "SUCCESS: ".
Messages are separated by "----------------" on a new line.

packages/sheets_addon/Code.js
@@ -119,6 +119,7 @@ class GoogleSheetsDevEnvironment {
 
     constructor(currentSheetName, opt) {
         this.currentSheetName = currentSheetName;
+        this.noteMsgs = new Map(); // :Map<string, Msg[]>, indexed by row_col
         this.noteStylers = new Map(); // :Map<string, NoteStyler>
         this.bgColorStylers  = new Map(); // :Map<string, BackgroundColorStyler>
         this.fontColorStylers = new Map(); // :Map<string, FontColorStyler>
@@ -130,13 +131,12 @@ class GoogleSheetsDevEnvironment {
     }
 
     message(msg) {
-        
         if (msg.sheet != this.currentSheetName) return;
 
         switch (msg.tag) {
-            case "error":       return this.markNote(msg);
-            case "warning":     return this.markNote(msg);
-            case "success":     return this.markNote(msg);
+            case "error":       return this.collectNoteMsgs(msg);
+            case "warning":     return this.collectNoteMsgs(msg);
+            case "success":     return this.collectNoteMsgs(msg);
             case "comment":     return this.markComment(msg);
             case "header":      return this.markHeader(msg);
             case "content":     return this.markContent(msg);
@@ -161,6 +161,41 @@ class GoogleSheetsDevEnvironment {
         return values;
     }
 
+    collectNoteMsgs(msg) {
+        const msgIdx = `${msg.row}_${msg.col}`;
+        const msgs = this.noteMsgs.get(msgIdx);
+
+        if (msgs == undefined) {
+            const msgs = Array.of(msg);
+            this.noteMsgs.set(msgIdx, msgs);
+            return;
+        }
+
+        if (msg.tag == 'error')
+            msgs.unshift(msg);
+        else if (msg.tag == 'warning')
+            msgs.push(msg);
+        // drop extra msgs with msg.tag == 'success' because there should
+        // only have been one message on success!
+    }
+
+    msgs2msg(msgs) {
+        if (!msgs.length)
+            return undefined;
+
+        const msg = {};
+        msg.tag = msgs[0].tag;
+        msg.row = msgs[0].row;
+        msg.col = msgs[0].col;
+        msg.longMsg = "";
+
+        for (const m of msgs) {
+            msg.longMsg += m.tag.toUpperCase() + ": " + m.longMsg + 
+                            "\n----------------\n";
+        }
+        return msg;
+    }
+
     markNote(msg) {
         const color = NOTE_COLORS.get(msg.tag) || DEFAULT_BG_COLOR;
         let noteStyler = this.noteStylers.get(msg.longMsg);
@@ -172,7 +207,6 @@ class GoogleSheetsDevEnvironment {
     }
 
     markComment(msg) {
-
         let colorStyler = this.fontColorStylers.get(COMMENT_FONT_COLOR);
         if (colorStyler == undefined) {
             colorStyler = new FontColorStyler(COMMENT_FONT_COLOR);
@@ -183,7 +217,6 @@ class GoogleSheetsDevEnvironment {
     }
 
     markHeader(msg) {
-
         let bgColorStyler = this.bgColorStylers.get(msg.color);
         if (bgColorStyler == undefined) {
             bgColorStyler = new BackgroundColorStyler(msg.color);
@@ -195,9 +228,7 @@ class GoogleSheetsDevEnvironment {
         this.centerStyler.addCell(msg);
     }
 
-
     markContent(msg) {
-
         let bgColorStyler = this.bgColorStylers.get(msg.color);
         if (bgColorStyler == undefined) {
             bgColorStyler = new BackgroundColorStyler(msg.color);
@@ -215,19 +246,16 @@ class GoogleSheetsDevEnvironment {
     }
 
     markOp(msg) {
-
         this.boldStyler.addCell(msg);
         this.centerStyler.addCell(msg);
     }
     
-    markSymbol(msg) {
-        
+    markSymbol(msg) {    
         this.boldStyler.addCell(msg);
         this.centerStyler.addCell(msg);
     }
 
     highlight() {
-
         let spreadsheet = SpreadsheetApp.getActive();
         let sheet = spreadsheet.getActiveSheet();
         sheet.clearNotes();
@@ -243,6 +271,10 @@ class GoogleSheetsDevEnvironment {
         this.boldStyler.applyToSheet(sheet);
         this.borderStyler.applyToSheet(sheet);
         this.centerStyler.applyToSheet(sheet);
+
+        for (const msgs of this.noteMsgs.values()) {
+            this.markNote(this.msgs2msg(msgs))
+        }
 
         for (const styler of this.noteStylers.values()) {
             styler.applyToSheet(sheet);

commit acd8258a29f60b5dc86bb8fb3852e5dc4c6cf5d4

Fixed a bug repeating error/warning messages in Note text in Sheets Addon.
This bug was introduced in commit 0357e14b50205ccedfb0b55a431a58d9e6e4f873,
which lists all the errors/warnings for a cell.

In the situation where a sheet refers to symbols on other sheets, each secondary
sheet must be loaded to resolve symbols. This symbol resolution is recursive,
so the main sheet is processed multiple times. Each time it is processed, any
errors encountered and reported again. Because of an error in the implementation
of the notes messages, multiple copies of the error were reported in the note -
once for each sheet processed during symbol resolution.

This fix now deduplicates the messages when they are stored.

I added a test in testSourceMulti the demonstrates how this issue can arise, but
the bug doesn't reveal itself in the source tests because it is DevEnvironment
code dependent, and the Sheets Addon has its own dev environment where the bug
was introduced.

packages/interpreter/src/interpreter.ts
@@ -334,7 +334,7 @@ export class Interpreter {
         const pass = new ExecuteTests(symbols, recursive);
 
         pass.getEnvAndTransform(targetGrammar, this.opt)
-            .msgTo(m => this.devEnv.message(m));
+            .msgTo(m => sendMsg(this.devEnv, m));
     }
 }
 
@@ -372,7 +372,7 @@ function addSheet(
     const sheet = new Worksheet(sheetName, cells);
     project.sheets[sheetName] = sheet;
     project.grammar = SOURCE_PASSES.getEnvAndTransform(project, devEnv.opt)
-                                     .msgTo(m => devEnv.message(m));
+                                     .msgTo(m => sendMsg(devEnv, m));
     // check to see if any names didn't get qualified
     const [_, nameMsgs] =  SYMBOL_PASSES.getEnvAndTransform(project.grammar, devEnv.opt)
                                         .destructure();

packages/sheets_addon/Code.js
@@ -114,11 +114,20 @@ class BackgroundColorStyler extends Styler {
 }
 
 
+// Holds all unique messages for a cell indexed by longMsg.
+class NoteMsgs {
+    constructor() {
+        this.error = new Map();
+        this.warning = new Map();
+        this.success = new Map();
+    }
+}
+
 class GoogleSheetsDevEnvironment {
 
     constructor(currentSheetName, opt) {
         this.currentSheetName = currentSheetName;
-        this.noteMsgs = new Map(); // :Map<string, Msg[]>, indexed by row_col
+        this.noteMsgs = new Map(); // :Map<string, NoteMsgs>, indexed by row_col
         this.noteStylers = new Map(); // :Map<string, NoteStyler>
         this.bgColorStylers  = new Map(); // :Map<string, BackgroundColorStyler>
         this.fontColorStylers = new Map(); // :Map<string, FontColorStyler>
@@ -162,36 +171,33 @@ class GoogleSheetsDevEnvironment {
 
     collectNoteMsgs(msg) {
         const msgIdx = `${msg.row}_${msg.col}`;
-        const msgs = this.noteMsgs.get(msgIdx);
+        let msgs = this.noteMsgs.get(msgIdx);
 
         if (msgs == undefined) {
-            const msgs = Array.of(msg);
+            msgs = new NoteMsgs();
             this.noteMsgs.set(msgIdx, msgs);
-            return;
         }
 
-        if (msg.tag == 'error')
-            msgs.unshift(msg);
-        else if (msg.tag == 'warning')
-            msgs.push(msg);
-        // drop extra msgs with msg.tag == 'success' because there should
-        // only have been one message on success!
+        msgs[msg.tag].set(msg.longMsg, msg);
     }
 
-    msgs2msg(msgs) {
-        if (!msgs.length)
-            return undefined;
+    msgs2msg(msgs) {    // msgs is a NoteMsgs object
+        let msg = undefined;
 
-        const msg = {};
-        msg.tag = msgs[0].tag;
-        msg.row = msgs[0].row;
-        msg.col = msgs[0].col;
-        msg.longMsg = "";
-
-        for (const m of msgs) {
-            msg.longMsg += m.tag.toUpperCase() + ": " + m.longMsg + 
-                            "\n----------------\n";
+        for (const t in msgs) {
+            for (const m of msgs[t].values()) {
+                if (msg === undefined) {
+                    msg = Object.create(null);
+                    msg.tag = m.tag;
+                    msg.row = m.row;
+                    msg.col = m.col;
+                    msg.longMsg = "";
+                }
+                msg.longMsg += m.tag.toUpperCase() + ": " + m.longMsg + 
+                                "\n----------------\n";
+            }
         }
+
         return msg;
     }
 
@@ -282,10 +288,9 @@ class GoogleSheetsDevEnvironment {
 
     logDebug(...msgs) {
         gramble.logDebug(this.opt.verbose, ...msgs)
+        alert(`logDebug: ${JSON.stringify(msgs)}`);
     }
 }
-
-
 
 /*
 function showDialog(htmlString: string, title: string = ""): void {

packages/tests/source/multi/testSourceMulti.ts
@@ -1,3 +1,4 @@
+import { VERBOSE_DEBUG } from "../../../interpreter/src/utils/logging.js";
 import {
     testSource, SourceTest, 
     Error, Warning 
@@ -306,7 +307,7 @@ describe(`Source ${DIR}`, function() {
             "multi17external.All",
         ],
     });
-    
+
     testSrc({
 		desc: '17d. Same as 17a but references both uppercase',
         symbol: "result",
@@ -323,6 +324,31 @@ describe(`Source ${DIR}`, function() {
             "multi17external.w3",
             "multi17external.All",
         ],
+    });
+    
+    testSrc({
+		desc: '18. More complex multi-sheet example with multiple errors',
+        symbol: "Stem",
+        results: [
+            {text: "fooable"},
+        ],
+        errors: [
+            Error(3, 0, "Invalid operator: 'testX:'"),
+            Warning(6, 0, "Assigning temporary symbol name: '$Auto7'"),
+            Error(7, 1, "Failed unit test - no matching outputs"),
+        ],
+        symbols: [
+            "All",
+            "multi18.All",
+            "multi18.Stem",
+            "multi18.$Auto7",
+            "multi18-2.Root",
+            "multi18-2.Root2",
+            "multi18-2.All",
+            "multi18-3.Root",
+            "multi18-3.All",
+        ],
+        // verbose: VERBOSE_DEBUG,
     });
 
 });